db2d718f1618756c8ce425f85f239177e786d380
Abhinav306 <97822103+Abhinav306@users.noreply.github.com>
2024-11-26 15:51:55 +0530

    Update userAction.js

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -65,7 +65,7 @@ import {
   
       const config = { headers: { "Content-Type": "multipart/form-data" } };
   
-      const { data } = await axios.post(`http://localhost:4000/api/v1/register`, userData, config);
+      const { data } = await axios.post(`https://shopscape-o8gh.onrender.com/api/v1/register`, userData, config);
       localStorage.setItem("ai",JSON.stringify(data));
   
       dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
@@ -82,7 +82,7 @@ import {
     try {
       dispatch({ type: LOAD_USER_REQUEST });
   
-      const { data } = await axios.get(`http://localhost:4000/api/v1/me`);
+      const { data } = await axios.get(`https://shopscape-o8gh.onrender.com/api/v1/me`);
       
   
       dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
@@ -94,7 +94,7 @@ import {
   // Logout User
   export const logout = () => async (dispatch) => {
     try {
-      await axios.get(`http://localhost:4000/api/v1/logout`);
+      await axios.get(`https://shopscape-o8gh.onrender.com/api/v1/logout`);
   
       dispatch({ type: LOGOUT_SUCCESS });
     } catch (error) {
@@ -109,7 +109,7 @@ import {
   
       const config = { headers: { "Content-Type": "multipart/form-data" } };
   
-      const { data } = await axios.put(`http://localhost:4000/api/v1/me/update`, userData, config);
+      const { data } = await axios.put(`https://shopscape-o8gh.onrender.com/api/v1/me/update`, userData, config);
   
       dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
     } catch (error) {
@@ -128,7 +128,7 @@ import {
       const config = { headers: { "Content-Type": "application/json" } };
   
       const { data } = await axios.put(
-        `http://localhost:4000/api/v1/password/update`,
+        `https://shopscape-o8gh.onrender.com/api/v1/password/update`,
         passwords,
         config
       );
@@ -149,7 +149,7 @@ import {
   
       const config = { headers: { "Content-Type": "application/json" } };
   
-      const { data } = await axios.post(`http://localhost:4000/api/v1/password/forgot`, email, config);
+      const { data } = await axios.post(`https://shopscape-o8gh.onrender.com/api/v1/password/forgot`, email, config);
   
       dispatch({ type: FORGOT_PASSWORD_SUCCESS, payload: data.message });
     } catch (error) {
@@ -168,7 +168,7 @@ import {
       const config = { headers: { "Content-Type": "application/json" } };
   
       const { data } = await axios.put(
-        `http://localhost:4000/api/v1/password/reset/${token}`,
+        `https://shopscape-o8gh.onrender.com/api/v1/password/reset/${token}`,
         passwords,
         config
       );
@@ -186,7 +186,7 @@ import {
   export const getAllUsers = () => async (dispatch) => {
     try {
       dispatch({ type: ALL_USERS_REQUEST });
-      const { data } = await axios.get(`http://localhost:4000/api/v1/admin/users`);
+      const { data } = await axios.get(`https://shopscape-o8gh.onrender.com/api/v1/admin/users`);
   
       dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
     } catch (error) {
@@ -198,7 +198,7 @@ import {
   export const getUserDetails = (id) => async (dispatch) => {
     try {
       dispatch({ type: USER_DETAILS_REQUEST });
-      const { data } = await axios.get(`http://localhost:4000/api/v1/admin/user/${id}`);
+      const { data } = await axios.get(`https://shopscape-o8gh.onrender.com/api/v1/admin/user/${id}`);
   
       dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
     } catch (error) {
@@ -214,7 +214,7 @@ import {
       const config = { headers: { "Content-Type": "application/json" } };
   
       const { data } = await axios.put(
-        `http://localhost:4000/api/v1/admin/user/${id}`,
+        `https://shopscape-o8gh.onrender.com/api/v1/admin/user/${id}`,
         userData,
         config
       );
@@ -233,7 +233,7 @@ import {
     try {
       dispatch({ type: DELETE_USER_REQUEST });
   
-      const { data } = await axios.delete(`http://localhost:4000/api/v1/admin/user/${id}`);
+      const { data } = await axios.delete(`https://shopscape-o8gh.onrender.com/api/v1/admin/user/${id}`);
   
       dispatch({ type: DELETE_USER_SUCCESS, payload: data });
     } catch (error) {
